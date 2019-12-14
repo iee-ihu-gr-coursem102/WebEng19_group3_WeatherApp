7f9c5e4d098a4ef66c4a446b4fde1d5e3db65202
olgaloia1986 <olgaloia@yahoo.gr>
2019-12-14 13:37:49 +0200

    login page new

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Login from './login_page/Login'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 class App extends React.Component {
   render() {
-      return(
-        <Login/>
+      return(<Router>
+        <div className="App">
+          <div className="auth-wrapper">
+            <div className="auth-inner">
+              <Switch>
+                <Route exact path='/' component={Login} />
+                <Route path="/sign-in" component={Login} />
+              </Switch>
+            </div>
+          </div>
+        </div></Router>
       )
   }
 }
--- src/login_page/Login.js
+++ src/login_page/Login.js
@@ -1,26 +1,55 @@
-var React = require('react');
+import React, { Component } from "react";
 
-export default class Login extends React.Component {
+export default class Login extends Component {
+  constructor(props) {
+  super(props);
+  this.state = {
+    email: "",
+    password: "",
+    isLoading: false
+  };
+}
 
-  render() {
-      return(
-        <div className="container-fluid">
-          <div className="jumbotron col-sm-4 col-sm-offset-4 jumbotron login-form">
-            <div className="col-sm-12">
-              <div className="tect-center">
-                Weather App
-              </div>
-              <form>
-                <div>
-                  <label> Username:</label>
-                </div>
-                <div>
-                  <input/>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-      )
+  onSubmit() {
+    //----- call db
+    // let user = {
+    //   email: "petros@gmail.com",
+    //   password: "1234"
+    // }
+    console.log(this.state);
+    // if (this.state.email === this.user.email || this.state.password === this.user.password) {
+    //   console.log("correct user");
+    // }else {
+    //   console.log("wrong");
+    // }
   }
+  onTypeEmail(e){
+    this.setState({email: e.target.value});
+  }
+
+  onTypePassword(e){
+    this.setState({password: e.target.value});
+  };
+  onClick(){
+    console.log("click");
+  }
+    render() {
+        return (
+            <form onSubmit={this.onSubmit.bind(this)}>
+                <h3>Sign In</h3>
+
+                <div className="form-group">
+                    <label>Email address: </label>
+                    <input type="email" className="form-control" placeholder="Enter email" onChange={this.onTypeEmail.bind(this)}/>
+                </div>
+
+                <div className="form-group">
+                    <label>Password: </label>
+                    <input type="password" className="form-control" placeholder="Enter password" onChange={this.onTypePassword.bind(this)} />
+                </div>
+
+                <button type="submit" className="btn btn-primary btn-block" onClick={this.onClick.bind()}>Submit</button>
+            </form>
+        );
+    }
 }
