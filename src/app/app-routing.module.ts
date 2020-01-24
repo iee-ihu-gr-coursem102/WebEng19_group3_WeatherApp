49b88030cace0220c4cd04e701c951acadccf30e
olgaloia1986 <olgaloia@yahoo.gr>
2020-01-24 19:10:47 +0200

    app component routing and app store

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,49 @@
 import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
-const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+import { AuthPage } from './auth/auth.page';
+import { HomePage } from './home/home.page';
+import { AuthGuard } from './auth/auth.guard';
+import { SearchPage } from './search/search.page';
+import { FavoritePage } from './favorite/favorite.page';
+import { UserSettingsPage } from './user-settings/userSettings.page';
+
+const routes: Routes = [{
+  path: '',
+  redirectTo: 'auth',
+  pathMatch: 'full'
+},
+{
+  path: 'auth',
+  component: AuthPage
+},
+{
+  path: 'home',
+  component: HomePage,
+  canActivate: [AuthGuard]
+},
+{
+  path: 'search',
+  component: SearchPage,
+  canActivate: [AuthGuard]
+},
+{
+  path: 'favorite',
+  component: FavoritePage,
+  canActivate: [AuthGuard]
+},
+{
+  path: 'user-settings',
+  component: UserSettingsPage,
+  canActivate: [AuthGuard]
+}
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules,
+    useHash: true
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,60 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { StoreModule } from '@ngrx/store';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import * as fromApp from './store/app.reducer';
+import { AuthInterceptorService } from './auth/auth-interceptor.service';
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AuthService } from './auth/auth.service';
+import { AuthPage } from './auth/auth.page';
+import { HomePage } from './home/home.page';
+import { SearchPage } from './search/search.page';
+import { FavoritePage } from './favorite/favorite.page';
+import { FavotiteListPage } from './favorite/favorite-list/favorite-list.component';
+import { FavoriteItemPage } from './favorite/favorite-list/favorite-item/favorite-item.component';
+import { UserSettingsPage } from './user-settings/userSettings.page';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    AuthPage,
+    HomePage,
+    SearchPage,
+    FavoritePage,
+    UserSettingsPage,
+    FavotiteListPage,
+    FavoriteItemPage
+],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    StoreModule.forRoot(fromApp.appReducer),
+    StoreDevtoolsModule.instrument({ logOnly: environment.production }),
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
   providers: [
+    AuthService,
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true}
   ],
   bootstrap: [AppComponent]
 })
